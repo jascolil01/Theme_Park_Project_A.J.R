972f550e2663fc845f45e767ee66a5dbfe134544
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-02-21 14:46:16 -0500

    made structures for review details page and review card component

--- client/components/ReviewCard.jsx
+++ client/components/ReviewCard.jsx
@@ -2,9 +2,11 @@ import { useNavigate } from "react-router-dom"
 
 const ReviewCard = (props) => {
     const navigate = useNavigate()
-    
+    const handleClick = (id) => {
+        navigate(`/review/${id}`)
+    }
   return (
-    <div>
+    <div onClick={() => handleClick(props.id)}>
         <h1>{props.name}</h1>
     </div>
   )
