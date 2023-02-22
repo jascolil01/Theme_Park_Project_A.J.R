26c8776d50219f9f3753c4938955694e4b089287
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-02-22 10:13:42 -0500

    added basic form styling and structure

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,16 @@ import { useState, useEffect } from "react"
 import axios from 'axios'
 import ReviewCard from "../components/ReviewCard"
 import Header from "../components/Header"
+import AddReviewForm from "../components/AddReviewForm"
 
 
 
 const Home = () => {
     const [review, setReview] = useState([])
+    const [clicked, toggleClicked] = useState(false)
     const getReviews = async () => {
         const response = await axios.get('http://localhost:3001/api/review')
-        setReview(response.data.reviews) 
+        setReview(response.data.reviews)
         console.log(response.data.reviews)
     }
 
@@ -18,40 +20,51 @@ const Home = () => {
     }, [])
 
     const handleClick = () => {
-        
+        if (clicked === false) {
+            toggleClicked(true)
+        }
+        else {
+            toggleClicked(false)
+        }
     }
 
     return (
         <div>
             <Header />
-        <div className="home-page">
-            <header className="home-header">
-                <div className="head">
-                    <div className="head-container">
-                        <div className="head-text">
+            <div className="home-page">
+                <header className="home-header">
+                    <div className="head">
+                        <div className="head-container">
+                            <div className="head-text">
+                            </div>
                         </div>
                     </div>
-                </div>
-            </header>
-            <div className="entire-home">
-                <h1 className="subtitle">Choose a Park</h1>
-                <button>Add Review</button>
-                <section className="container-grid">
-                    {review.map((review) => (
-                        <ReviewCard
-                            key={review._id}
-                            id={review._id}
-                            name={review.park}
-                            image={review.image}
-                            rating={review.overallRating}
-                            type={review.type}
-                        />
-                    ))}
-                </section>
+                </header>
+                <div className="entire-home">
+                    <h1 className="subtitle">Choose a Park</h1>
+                    
+                    {clicked ? (
+                        <div>
+                        <button onClick={() => handleClick()}>Close</button>
+                        <AddReviewForm getReviews={getReviews}/>
+                        </div>
+                    ) : <button onClick={() => handleClick()}>Add Review</button>}
+                    <section className="container-grid">
+                        {review.map((review) => (
+                            <ReviewCard
+                                key={review._id}
+                                id={review._id}
+                                name={review.park}
+                                image={review.image}
+                                rating={review.overallRating}
+                                type={review.type}
+                            />
+                        ))}
+                    </section>
 
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
