e146c1d4917d29929f136f4f9ccddd35c2d42672
jascolil01 <jayasco246@gmail.com>
2023-02-22 09:14:19 -0500

    fixed coflicting header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,15 +5,11 @@ function Header() {
   return (
     <div className="header">
 
-      <div className="logo-div">
-        <img className='logo' src="https://i.imgur.com/MlsbVKq.jpeg" alt="Logo" />
-        <img className='banner' src='https://i.imgur.com/oiuZJmO.jpeg' alt='Banner' />
-
       <div className="logo">
         <img src="https://i.imgur.com/jKvsXlY.gif" alt="Banner" />
       </div>
       <div className="title">
-        
+
       </div>
     </div>
   );
