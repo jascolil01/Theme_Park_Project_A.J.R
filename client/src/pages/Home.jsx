784cdaef53e43650d7a397a15c33558ac91f1055
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-02-23 15:27:57 -0500

    deleted console logs

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,7 +14,6 @@ const Home = () => {
     const getReviews = async () => {
         const response = await axios.get('/api/review')
         setReview(response.data.reviews)
-        console.log(response.data.reviews)
     }
 
     const handleToggleDarkMode = () => {
